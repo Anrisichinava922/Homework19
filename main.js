4ceab83f36266fe1cd9b220d73ff322d23092875
Anrisichinava922 <anrisichinava922@gmail.com>
2023-11-24 20:03:06 +0400

    Homework19

--- main.js
+++ main.js
@@ -2,11 +2,8 @@ const days = document.querySelector('#days')
 const hours = document.querySelector('#hours')
 const minutes = document.querySelector('#minutes')
 const seconds = document.querySelector('#seconds')
-
 const currentYear = new Date().getFullYear()
-
 const newYearTime = new Date(`December 31 ${currentYear} 00:00:00`);
-
 function updateCountdowntime(){
     const currentTime = new Date();
     const diff = newYearTime - currentTime
@@ -14,7 +11,6 @@ function updateCountdowntime(){
     const h = Math.floor(diff / 1000 / 60 / 60) %24;
     const m = Math.floor(diff / 1000 / 60) % 60;
     const s = Math.floor(diff / 1000) % 60;
-
     days.innerHTML = d;
     hours.innerHTML = h < 10 ? '0' + h : h;
     minutes.innerHTML = m < 10 ? '0' + m : m;
